16786f3afaf76d5f778450e0fa96848a3f29a9cc
ArchPalmieri <daniel.phil.palmieri@gmail.com>
2021-07-29 16:33:25 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29605926831289153,
+          "pitch": 0.15606025740584073,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 1.7295991060942786,
+          "pitch": 0.09673482343476003,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2268605431960644,
+          "pitch": 0.13482826199705755,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": -1.754098333182725,
+          "pitch": 0.10564648608772487,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16004037937438298,
+          "pitch": 0.08807586351738195,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.500337131679025,
+          "pitch": 0.09218675109915964,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.050904303591583044,
+          "pitch": -0.4629846841461962,
+          "title": "Title",
+          "text": "Controsottiffo H.260<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Mannarino's Showroom ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
